d25265dd1507c021a8fa29175bebcf98593f72ad
Oleksii Chornenkyi <oleksiy.chornenkyy@gmail.com>
2013-07-04 17:42:12 +0300

    changed url

--- tests/tests/localway_test.py
+++ tests/tests/localway_test.py
@@ -9,7 +9,7 @@ __author__ = 'lxz'
 class LocalwayTest(WTFBaseTest):
 
     webdriver = WTF_WEBDRIVER_MANAGER.new_driver()
-    webdriver = WTF_WEBDRIVER_MANAGER.new_driver()
+    webdriver.get("http://172.31.237.12/")
     google_page = PageFactory.create_page(HomePage, webdriver)
     google_page.search_for_what("1223456")
     google_page.search_for_where("1223456")
